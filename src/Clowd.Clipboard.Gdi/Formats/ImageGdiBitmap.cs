0efc896c74b152ed845103b6503e8ff7e6d47505
Caelan Sayler <git@caesay.com>
2022-10-06 11:22:21 +0100

    Add missed XML comments

--- src/Clowd.Clipboard.Gdi/Formats/ImageGdiBitmap.cs
+++ src/Clowd.Clipboard.Gdi/Formats/ImageGdiBitmap.cs
@@ -8,22 +8,30 @@ using System.Threading.Tasks;
 
 namespace Clowd.Clipboard.Formats;
 
+/// <summary>
+/// Converts bitmap bytes to a specific encoder format: eg. PNG or TIFF
+/// </summary>
 [SupportedOSPlatform("windows")]
 public class ImageGdiBitmap : BytesDataConverterBase<Bitmap>
 {
     private readonly ImageFormat format;
 
+    /// <summary>
+    /// Creates a <see cref="ImageGdiBitmap"/> with the specified encoding.
+    /// </summary>
     public ImageGdiBitmap(ImageFormat format)
     {
         this.format = format;
     }
 
+    /// <inheritdoc/>
     public override Bitmap ReadFromBytes(byte[] data)
     {
         using var ms = new MemoryStream(data);
         return (Bitmap)Bitmap.FromStream(ms);
     }
 
+    /// <inheritdoc/>
     public override byte[] WriteToBytes(Bitmap obj)
     {
         using var ms = new MemoryStream();
