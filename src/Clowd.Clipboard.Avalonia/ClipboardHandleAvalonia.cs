7d68708f7528a8491e0dad62dd71cb93c52ec410
Caelan Sayler <git@caesay.com>
2022-10-09 01:07:34 +0100

    Use pre-multiplied alpha for Avalonia bitmaps.

--- src/Clowd.Clipboard.Avalonia/ClipboardHandleAvalonia.cs
+++ src/Clowd.Clipboard.Avalonia/ClipboardHandleAvalonia.cs
@@ -28,14 +28,14 @@ public class ClipboardHandleAvalonia : ClipboardHandleGdiBase, IClipboardHandleP
         var bitmapData = gdi.LockBits(
             new Rectangle(0, 0, gdi.Width, gdi.Height),
             ImageLockMode.ReadOnly,
-            PixelFormat.Format32bppArgb);
+            PixelFormat.Format32bppPArgb);
 
         var bmp = new AvaBitmap(
             Avalonia.Platform.PixelFormat.Bgra8888,
-            Avalonia.Platform.AlphaFormat.Unpremul,
+            Avalonia.Platform.AlphaFormat.Premul,
             bitmapData.Scan0,
             new Avalonia.PixelSize(bitmapData.Width, bitmapData.Height),
-            new Avalonia.Vector(96, 96),
+            new Avalonia.Vector(gdi.HorizontalResolution, gdi.VerticalResolution),
             bitmapData.Stride);
 
         gdi.UnlockBits(bitmapData);
